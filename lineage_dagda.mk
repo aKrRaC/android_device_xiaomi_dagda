7e36382b86dc9f6f17c1fcdce0094eeb10813a90
aKrRaC <akhilrac2000@gmail.com>
2024-08-15 22:36:53 +0530

    dagda: Riseup dagda

--- lineage_dagda.mk
+++ lineage_dagda.mk
@@ -7,7 +7,7 @@
 # Inherit from dagda device
 $(call inherit-product, device/xiaomi/dagda/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common RisingOSS configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_dagda
@@ -22,8 +22,23 @@ PRODUCT_SYSTEM_DEVICE := zeus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="zeus_global-user 14 UKQ1.230917.001 V816.0.4.0.ULBMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    RISING_CHIPSET="Snapdragon 8 Gen 1" \
+    RISING_MAINTAINER="aKrRaC"
 
 BUILD_FINGERPRINT := Xiaomi/zeus_global/zeus:14/UKQ1.230917.001/V816.0.4.0.ULBMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+# Enable blur support
+TARGET_ENABLE_BLUR := true
+# whether to ship aperture camera
+PRODUCT_NO_CAMERA := false
+# GMS build flags
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+# Whether to ship pixel launcher and set it as default launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
